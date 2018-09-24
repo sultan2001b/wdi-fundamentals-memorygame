98664c223b3e910ba0520169d9ed7bccb15616f7
sultan <sultan2001b@gmail.com>
2018-09-24 17:47:29 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,20 +3,28 @@
 var cards=["queen", "queen", "king", ,"king"];
 
 var cardsInPlay=[];
-var cardOne=cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo=cards[2];
-cardsInPlay.push(cardTwo);
-if(cardsInPlay.length === 2)
-{
-	if(cardsInPlay[0]===cardsInPlay[1])
-	{
-		alert("You found a match!");
 
-	}
-	else
+var checkForMatch=function()
+{
+if(cardsInPlay[0]===cardsInPlay[1])
+		{
+			alert("You found a match!");
+		}
+		else
+		{
+			alert("Sorry, try again.");
+		}
+}
+
+var flipCard=function(cardId)
+{
+	console.log("user flipped "+cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
+	if(cardsInPlay.length === 2)
 	{
-		alert("Sorry, try again.");
-		
+		checkForMatch();
 	}
 }
+flipCard(0);
+flipCard(2);
